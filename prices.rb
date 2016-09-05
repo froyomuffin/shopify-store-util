38bed56988bb35672692380110c3b556f191fc1f
Thomas Wang <froyomuffin@gmail.com>
2016-09-05 16:22:30 -0400

    Made type filtering case insensitive and printout formatting

--- prices.rb
+++ prices.rb
@@ -11,10 +11,10 @@ class StoreProcessor
     end
 
     def getFilteredTotal(itemTypes) 
+        puts "Getting total for item types #{itemTypes}"
+
         page = 0
         total = 0
-
-        puts "Getting total for item types #{itemTypes}"
 
         loop do # This part could be done more elegantly if we knew ahead of time the number of pages. We could build map and filter directly from the pages and avoid this loop 
             page += 1
@@ -28,12 +28,12 @@ class StoreProcessor
             total +=
             products
                 .select do |product| # Filter for clocks and watches
-                    itemTypes.any? { |filter| product["product_type"].downcase.include? filter }
+                    itemTypes.any? { |type| product["product_type"].downcase.include? type.downcase }
                 end
                 .flat_map do |product| # Extract prices of each variant 
                     puts product["title"]
                     product["variants"].map do |variant| 
-                        puts "\t #{variant["title"]} -> #{variant["price"]}"
+                        puts "  #{variant["title"]} -> $#{variant["price"]}"
                         variant["price"].to_f
                     end
                 end
@@ -42,16 +42,18 @@ class StoreProcessor
 
         total = total.round(2)
 
-        puts "TOTAL: #{'%.2f' % total}" # Some extra formatting to make sure we get exactly two decimals
+        puts "TOTAL: $#{'%.2f' % total}" # Some extra formatting to make sure we get exactly two decimals
         return total
     end
 end
 
 uri = URI('http://shopicruit.myshopify.com/')
-Types = [ "clock" ]
 
 processor = StoreProcessor.new(uri)
-processor.getFilteredTotal(Types);
-processor.getFilteredTotal([]);
-processor.getFilteredTotal(["Elephants"]);
-processor.getFilteredTotal([ "clock", "watch" ]);
+
+Types = [ "clock" ]
+processor.getFilteredTotal(Types); puts
+processor.getFilteredTotal([]); puts
+processor.getFilteredTotal(["Elephants"]); puts
+processor.getFilteredTotal([ "clock", "watch" ]); puts
+processor.getFilteredTotal([ "clocK", "wAtch" ]); puts
