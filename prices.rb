820d8212a87571449253074d9b7daf3d0d460b46
Thomas Wang <tw@froyomuffin.com>
2016-09-10 20:51:53 -0400

    More rubocop

--- prices.rb
+++ prices.rb
@@ -4,45 +4,43 @@ require 'net/http'
 require 'json'
 
 class StoreProcessor
-  public
-
   def initialize(store_uri)
     if store_uri.class != URI::HTTP
       raise ArgumentError, 'Failed to construct StoreProcessor: No URI::HTTP object specified!'
     end
 
-    @PRODUCTS_URI = store_uri
-    @PRODUCTS_URI.path = "/products.json"
+    @product_uri = store_uri
+    @product_uri.path = '/products.json'
   end
 
-  def get_filtered_total(item_types) 
-    puts "Getting total for item types #{item_types} from #{@PRODUCTS_URI.host}"
+  def get_filtered_total(item_types)
+    puts "Getting total for item types #{item_types} from #{@product_uri.host}"
 
     page = 0
     total = 0
 
-    loop do # This part could be done more elegantly if we knew ahead of time the number of pages. We could build map and filter directly from the pages and avoid this loop 
+    loop do # This part could be done more elegantly if we knew ahead of time the number of pages. We could build map and filter directly from the pages and avoid this loop
       page += 1
-      params = { :page => page }
-      @PRODUCTS_URI.query = URI.encode_www_form(params)
+      params = { page: page }
+      @product_uri.query = URI.encode_www_form(params)
 
       products = fetch_products
 
-      break if products == nil || products.empty?
+      break if products.nil? || products.empty?
 
       total +=
       products
       .select do |product| # Filter for clocks and watches
         item_types.any? { |type| product['product_type'].downcase.include? type.downcase }
       end
-      .flat_map do |product| # Extract prices of each variant 
+      .flat_map do |product| # Extract prices of each variant
         puts product['title']
-        product['variants'].map do |variant| 
+        product['variants'].map do |variant|
           puts "  #{variant['title']} -> $#{variant['price']}"
           variant['price'].to_f
         end
       end
-      .inject(0) { |sum, price| sum + price } # Sum the prices from all the item variants filtered
+      .reduce(0) { |a, e| a + e } # Sum the prices from all the item variants filtered
     end
 
     puts "TOTAL: $#{format('%.2f', total)}" # Some extra formatting to make sure we get exactly two decimals
@@ -52,7 +50,7 @@ class StoreProcessor
   private
 
   def fetch_products
-    response = Net::HTTP.get_response(@PRODUCTS_URI)
+    response = Net::HTTP.get_response(@product_uri)
     response.value # Throws an exception if the response code is not 2xx
 
     products = JSON.parse(response.body)['products']
