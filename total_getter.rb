ebce99cf7fad9a6da0d349cbffc478a318ef9d43
Thomas Wang <tw@froyomuffin.com>
2016-09-11 21:30:56 -0400

    Added rudimentary commandline script to use StoreProcessor on any Shopify store

---
+++ total_getter.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+require 'optparse'
+require 'ostruct'
+require_relative 'StoreProcessor'
+
+# TODO: Formatting, safety, cleanup
+options = OpenStruct.new
+options.types = Array.new
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{File.basename(__FILE__)} [options]"
+  opts.separator ""
+  opts.separator "Specific options:"
+
+  opts.on("-s", "--store STORE", "Store URI") do |store_uri|
+    options.store_uri = store_uri
+  end
+
+  opts.on("--types x y z", Array, "Item types") do |types|
+    options.types.push(*types)
+  end
+
+  opts.on("-f", "--type TYPE", "Item types") do |type|
+    options.types.push(type)
+  end
+
+  opts.on_tail("-h", "--help", "Help") do
+    puts opts.banner
+  end
+end.parse!
+
+StoreProcessor.new(options.store_uri)
+  .get_filtered_total(options.types)
